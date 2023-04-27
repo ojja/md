d092df64f7c3fb3df48ee050ce6fd7163f6dd553
Ojja <ojja@Tonys-MacBook-Pro-2.local>
2023-04-27 14:37:30 +0200

    recent viewed

--- app/components/RecentlyViewedProducts.tsx
+++ app/components/RecentlyViewedProducts.tsx
@@ -1,4 +1,5 @@
 import useRecentView from "~/stores/wishtList";
+import { ProductWidget } from "./product/ProductWidget";
 
 export default function RecentlyViewedProducts() {
     const {
@@ -9,17 +10,13 @@ export default function RecentlyViewedProducts() {
         <div className="mt-10 bg-white border-t-2">
             <div className="container px-4 py-16 mx-auto sm:py-24 sm:px-6">
                 <h2 className="mb-5 text-2xl font-bold tracking-tight text-gray-900">Recently Viewed Products</h2>
-                <div className="flex">
-                    {recentItems.map((product) => (
-                        <div>
-                            <img
-                                src={product.thumbnail}
-                                alt={product.slug}
-                                className="object-cover object-center w-full h-full"
-                            />
-                            <div key={product.id}>{product.name}</div>
-                        </div>
-                    ))}
+                <div className="grid grid-cols-1 mt-6 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-5 xl:gap-x-8">
+                    
+                    {recentItems.sort(() => Math.random() - 0.5)
+                        .slice(0, 5)
+                        .map((productData: any) => (
+                            <ProductWidget product={productData} key={productData.id} />
+                        ))}
                 </div>
             </div>
         </div>
