00adcff730bfb71c50e065ab2899e7685597cf5d
Ojja <georgesaad111@gmail.com>
2023-05-07 10:42:50 +0300

    md comments

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-import { createEventHandler } from "@remix-run/cloudflare-workers";
+// import { createEventHandler } from "@remix-run/cloudflare-workers";
+// import * as build from "@remix-run/dev/server-build";
+
+// addEventListener(
+//   "fetch",
+//   createEventHandler({ build, mode: process.env.NODE_ENV })
+// );
+
+import { createRequestHandler } from "@remix-run/netlify";
 import * as build from "@remix-run/dev/server-build";
 
-addEventListener(
-  "fetch",
-  createEventHandler({ build, mode: process.env.NODE_ENV })
-);
+export const handler = createRequestHandler({
+  build,
+  mode: process.env.NODE_ENV,
+});
