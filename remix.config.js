6a1b9ee56e7a76b718e7b012f231e4e9bbd6e49d
Ojja <georgesaad111@gmail.com>
2023-05-24 15:10:04 +0300

    test cart

--- remix.config.js
+++ remix.config.js
@@ -25,7 +25,7 @@ withEsbuildOverride((option, { isServer }) => {
 /** @type {import('@remix-run/dev').AppConfig}*/
 module.exports = {
   devServerBroadcastDelay: 1000,
-  devServerPort: 8080,
+  devServerPort: 3000,
   ...(process.env.NODE_ENV === "production" ? {
     serverBuildTarget: "cloudflare-pages",
     serverConditions: ["worker"],
