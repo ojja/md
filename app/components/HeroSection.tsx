27401d49733a87445efd961cf76b1954b87bd6b1
Ojja <georgesaad111@gmail.com>
2023-07-02 16:43:38 +0300

    pwa updates

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -4,15 +4,15 @@ export default function HeroSection() {
     const { t } = useTranslation();
     return (
         <>
-            <div className="relative isolate px-6 pt-14 lg:px-8">
+            <div className="relative px-6 isolate pt-14 lg:px-8">
                 <img
-                    src="https://images.unsplash.com/photo-1679150903021-6571b98b4afe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80"
+                    src="/images/hero.webp"
                     alt=""
-                    className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
+                    className="absolute inset-0 object-cover object-right w-full h-full -z-10 md:object-center"
                 />
-                <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:py-28">
+                <div className="max-w-2xl py-16 mx-auto sm:py-24 lg:py-28">
                     <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-                        <div className="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                        <div className="relative px-3 py-1 text-sm leading-6 text-gray-600 rounded-full ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                             Announcing our next round of funding.{' '}
                             <a href="#" className="font-semibold text-primary-600">
                                 <span className="absolute inset-0" aria-hidden="true" />
@@ -28,7 +28,7 @@ export default function HeroSection() {
                             Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
                             fugiat veniam occaecat fugiat aliqua.
                         </p>
-                        <div className="mt-10 flex items-center justify-center gap-x-6">
+                        <div className="flex items-center justify-center mt-10 gap-x-6">
                             <a
                                 href="#"
                                 className="rounded-md bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
--- app/components/HeroTitle.js
+++ app/components/HeroTitle.js
@@ -1,11 +1,11 @@
 export default function HeroTitle() {
     return (
         <>
-            <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+            <div className="relative py-24 overflow-hidden bg-gray-900 isolate sm:py-32">
                 <img
-                    src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+                    src="/images/hero.webp"
                     alt=""
-                    className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
+                    className="absolute inset-0 object-cover object-right w-full h-full -z-10 md:object-center"
                 />
                 <svg
                     viewBox="0 0 1097 845"
@@ -55,8 +55,8 @@ export default function HeroTitle() {
                         </linearGradient>
                     </defs>
                 </svg>
-                <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                    <div className="mx-auto max-w-2xl lg:mx-0">
+                <div className="px-6 mx-auto max-w-7xl lg:px-8">
+                    <div className="max-w-2xl mx-auto lg:mx-0">
                         <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Hero Title</h1>
                         <p className="mt-6 text-lg leading-8 text-gray-300">
                             Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
